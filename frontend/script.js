0f9392db6eec021cd3a3912f58b39ad02fe38f3b
akkineni aasritha <aasrithaakkineni@gmail.com>
2026-06-15 11:01:41 +0530

    Completed frontend table display

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,20 @@
 function showMessage() {
     alert("Displaying processed ETL data");
 }
+
+window.onload = function () {
+    document.getElementById("stockTable").innerHTML = `
+        <tr>
+            <td>1</td>
+            <td>Apple</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Tesla</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Microsoft</td>
+        </tr>
+    `;
+};
